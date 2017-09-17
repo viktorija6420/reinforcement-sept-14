8d420a34e8fe58f41bff6fccf535818ee075c498
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-17 12:12:53 -0400

    find date and attendees from each event

---
+++ exercise.rb
@@ -0,0 +1,14 @@
+events_per_city = [
+{id: "38fj8d900", city: 'Hamilton', events: [{date: '2017-01-01', attendees: 100}, {date: '2016-12-31', attendees: 60}]},
+{id: "39fo837y7", city: 'Toronto', events: [{date: '2017-03-30', attendees: 3000}, {date: '2017-07-07', attendees: 2500}, {date: '2017-02-04', attendees: 900}]},
+{id: "58uj8d800", city: 'Montreal', events: [{date: '2017-08-10', attendees: 250}]},
+{id: "48hn8d900", city: 'Kingston', events: [{ date: '2015-04-16', attendees: 45}]}
+]
+
+events_per_city.each do |h|
+ p h[:city]
+ events = h[:events]
+ events.each do |event|
+   p "Date:#{event[:date]}, #{event[:attendees]} people" 
+ end
+end
